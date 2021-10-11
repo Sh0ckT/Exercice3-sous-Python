678e072d7839064e4735e3d486b524a5908ca9dd
Sh0ckT <90909498+Sh0ckT@users.noreply.github.com>
2021-10-11 20:32:51 +0200

    Create Ex3.py

---
+++ Ex3.py
@@ -0,0 +1,17 @@
+a=int(input("Donner la valeur de a\n"))
+b=int(input("Donnez la valeur de b\n")) #chacun de ces trois permet de demander la valeur de a, b et c
+c=int(input("Donnez la valeur de c\n"))
+
+if(a > c and a > b):#verifie dans chacun des trois exemples suivant que a, b ou c est superieur au reste
+
+elif(b > c and b > a):
+
+    print("B est le plus grand")
+
+elif(c > b and c > a):
+
+      print("C est le plus grand")
+
+else:
+
+    print("ils sont tous égaux (/O_O)/")
